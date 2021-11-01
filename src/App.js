fb389bd7743928710d00696b818217b03eb62017
phu GKIM <diemduyen05@gmail.com>
2021-11-01 13:51:45 +0700

    first up source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+import FakeCardData from './context/FakeCardData';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FakeCardData>
+        <div className='cards'>
+          <ExpenseItem></ExpenseItem>
+          <ExpenseItem></ExpenseItem>
+          <ExpenseItem></ExpenseItem>
+          <ExpenseItem></ExpenseItem> 
+        </div>
+      </FakeCardData>
     </div>
   );
 }
