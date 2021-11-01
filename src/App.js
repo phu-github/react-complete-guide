9093ead16ebf8913dbd0562986c63113acf249d9
Phu Nguyen <nvphu1@tma.com.vn>
2021-11-01 17:42:23 +0700

    first up nvphu1 branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import ExpenseItem from './components/ExpenseItem';
 import FakeCardData from './context/FakeCardData';
 
 function App() {
+  const expense = [
+    { id: 'e1', title:'toilet Paper', amount:94.12, date: new Date(2020, 7, 14) },
+    { id: 'e2', title:'New TV', amount:94.12,date: new Date(2020, 7, 15) },
+    { id: 'e3', title:'Talk Show', amount:94.12, date: new Date(2020, 9, 2)},
+  ]
   return (
     <div className="App">
       <FakeCardData>
