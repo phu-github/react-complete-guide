899034de139d776d3e53d354cf477465c2b33f7f
Phu Nguyen <diemduyen05@gmail.com>
2022-07-05 23:50:42 +0700

    contex api

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,15 +2,18 @@ import React, {useContext} from 'react';
 import AuthenContext from '../../store/authen-context';
 import classes from './Navigation.module.css';
 const Navigation = (props) => {
-	const ctx = useContext(AuthenContext);
 	return (
-		<nav className={classes.nav}>
-			<ul>
-				{ctx.isLoggedIn && (<li><a href="/">Users</a></li>)}
-				{ctx.isLoggedIn && (<li><a href="/">Admin</a></li>)}
-				{ctx.isLoggedIn && (<li><button onClick={props.onLogout}>Logout</button></li>)}
-			</ul> 
-		</nav> 
+		<AuthenContext.Consumer>
+			{(ctx) => {return (
+				<nav className={classes.nav}>
+					<ul>
+						{ctx.isLoggedIn && (<li><a href="/">Users</a></li>)}
+						{ctx.isLoggedIn && (<li><a href="/">Admin</a></li>)}
+						{ctx.isLoggedIn && (<li><button onClick={props.onLogout}>Logout</button></li>)}
+					</ul> 
+				</nav> 
+			)}}
+		</AuthenContext.Consumer>
 	);
 };
 
